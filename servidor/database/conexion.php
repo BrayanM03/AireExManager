67ba264420c24affa5706933b65d2033e8abe92c
BrayanM03 <natsu5679@gmail.com>
2022-07-19 18:19:13 -0500

    Ok

--- servidor/database/conexion.php
+++ servidor/database/conexion.php
@@ -2,7 +2,7 @@
 $usuario = "root";
 $pass = "";
 try {
-    $con = new PDO('mysql:host=localhost;dbname=aire_express', $usuario, $pass);
+    $con = new PDO('mysql:host=localhost;dbname=aire_express;charset=utf8mb4', $usuario, $pass);
    
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
