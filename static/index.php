579167ef55dfecd53e884881a9b55187f0c3e6b2
BrayanM03 <natsu5679@gmail.com>
2022-08-03 19:17:30 -0500

    Ok

--- static/index.php
+++ static/index.php
@@ -15,7 +15,7 @@ if (empty($_SESSION["id"])) {
 	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
 	<meta name="author" content="AdminKit">
 	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
+	
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
